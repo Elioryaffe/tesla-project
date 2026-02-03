41c0ae74c7145f11bc69369ae8abe264fa3f837c
Elior <elior@gmail.com>
2026-02-03 20:38:22 +0200

    with data

---
+++ src/main/java/com/example/tesla/model/Car.java
@@ -0,0 +1,123 @@
+package com.example.tesla.model;
+
+import org.hibernate.validator.constraints.Length;
+
+import javax.persistence.*;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Entity
+@Table(name="car")
+
+public class Car implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotBlank
+    @Length(min = 2, max = 30)
+    private String brand;
+
+    @NotBlank
+    @Length(min = 2, max = 30)
+    private String model;
+
+    @Min(2000)
+    @Max(2030)
+    private Integer year;
+
+    @Min(100000)
+    private Double price;
+
+    @Min(100)
+    @Max(1000)
+    private Integer rangeKm;
+
+    @NotBlank
+    private String color;
+
+    private String imageUrl;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public Integer getYear() {
+        return year;
+    }
+
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Integer getRangeKm() {
+        return rangeKm;
+    }
+
+    public void setRangeKm(Integer rangeKm) {
+        this.rangeKm = rangeKm;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Car() {
+    }
+
+    public Car(Long id, String brand, String model, Integer year, Double price, Integer rangeKm, String color, String imageUrl) {
+        this.id = id;
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+        this.price = price;
+        this.rangeKm = rangeKm;
+        this.color = color;
+        this.imageUrl = imageUrl;
+    }
+}
