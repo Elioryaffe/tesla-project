ca3f64e075ef31cb072819a8859392ca4b8a729f
Elior <elior@gmail.com>
2026-02-09 11:43:14 +0200

    with fronted

---
+++ src/main/java/com/example/tesla/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.example.tesla.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("*")  // מאפשר מכל מקור (לפיתוח)
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*");
+    }
+}
